fc1ca03c0b1391df9ecfd6fd35e7e732b0dc287a
Kechi <bonifacekechi@gmail.com>
2021-04-29 18:59:49 -0400

    Solve problem in main.js and create README - installed npm package called 'to-words' - created readme with reference to npm package - use package to solve question - run test in node

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+// an npm package that converts numbers (including decimal points) into words.
+const { ToWords } = require('to-words')
+
 //problem setup instructions
 
 /* Convert an array of integers into an array of strings representing the phonetic equivalent of the
@@ -6,3 +9,21 @@
 Given an array: [3, 25, 209], print “Three,TwoFive,TwoZeroNine” into stdout.
 Given an array: [10, 300, 5], print “OneZero,ThreeZeroZero,Five” into stdout.
 */
+
+
+//an array of numbers that we use to set up the problem
+let numsArray = [ 7, 19, 23, 5, 4000]
+
+// we use the map function to iterate over the numsArray and assign it a new name called wordArray
+const wordArray = numsArray.map(words => {
+    // creating a new method will allow us to use the 'convert' property
+    const toWords = new ToWords()
+    // use the covert property, taking the parameter words to change our integer to the phonetic equivalent
+  let word1 = toWords.convert(words)
+  // the return stores and helps end the iteration. We also stringify our return. 
+  return word1.toString()
+    
+})
+
+// we log our function to run test in node
+console.log(wordArray)
---
+++ readme.md
@@ -0,0 +1,13 @@
+## Kargo Assessment by Kechi Boniface
+
+
+#  Question
+Convert an array of integers into an array of strings representing the phonetic equivalent of the integer.
+
+
+# NPM Package Used
+[Number to Words](https://www.npmjs.com/package/to-words)
+
+```
+npm i to-words
+```
